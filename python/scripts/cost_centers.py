d52b0fb3ff3095e74933cb64455f6401e9adb2a6
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-06-12 15:29:25 +0200

    Compare cost centers list

--- python/scripts/cost_centers.py
+++ python/scripts/cost_centers.py
@@ -2,5 +2,10 @@ import os
 from travelperk_http_python.builder.builder import build
 travelperk = build(os.environ["API_KEY"], False)
 cost_centers = travelperk.cost_centers().cost_centers().all()
-print(cost_centers)
-print(cost_centers.cost_centers[0])
+with open("/app/output/cost_centers", "a") as output_file:
+    output_file.write(str(cost_centers.offset))
+    output_file.write(str(cost_centers.limit))
+    output_file.write(str(len(cost_centers.cost_centers)))
+    output_file.write(str(cost_centers.cost_centers[0].id))
+    output_file.write(str(cost_centers.cost_centers[0].name))
+    output_file.write(str(cost_centers.cost_centers[0].count_users))
