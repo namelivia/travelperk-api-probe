08374572b5fc53107d94401562b421b4bb24ca89
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-06-27 14:25:37 +0200

    Fix naming

--- python/scripts/list_scim.py
+++ python/scripts/list_scim.py
@@ -2,6 +2,6 @@ import os
 from travelperk_http_python.builder.builder import build
 from utils import write_output
 travelperk = build(os.environ["API_KEY"], False)
-users = travelperk.scim().users().query().setStartIndex(1).setCount(10).get()
+users = travelperk.scim().users().query().set_start_index(1).set_count(10).get()
 write_output("scim_users", [
 ])
