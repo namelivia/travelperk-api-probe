3bbdc32146a1a61a3e3a08f6ab216e965ab114db
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-08-29 14:03:51 +0200

    Remove OAuth code, it wont be testeable automatically

--- php/scripts/run.php
+++ php/scripts/run.php
@@ -8,18 +8,9 @@ require_once("test_cost_centers.php");
 require_once("test_scim.php");
 require_once("test_users.php");
 use Namelivia\TravelPerk\ServiceProvider;
-use kamermans\OAuth2\Persistence\FileTokenPersistence;
 
 $sandboxTravelperkApiKey = (new ServiceProvider())->build(getenv("SANDBOX_API_KEY"), true);
 $travelperkApiKey = (new ServiceProvider())->build(getenv("API_KEY"), false);
-$travelperkOAuth = (new ServiceProvider())->buildOAuth2(
-    new FileTokenPersistence(getenv("ACCESS_TOKEN_PATH")),
-	getenv("CLIENT_ID"),
-	getenv("CLIENT_SECRET"),
-	getenv("REDIRECT_URL"),
-	[], # scopes
-	false
-);
 
 $tests = [
 	# Sandbox - Api key
@@ -31,17 +22,12 @@ $tests = [
 	new TripsTests($travelperkApiKey),
 	new TravelsafeTests($travelperkApiKey),
 	new UsersTests($travelperkApiKey),
-	# OAuth
-	new WebhooksTests($travelperkOAuth),
-	new TripsTests($travelperkOAuth),
-	new TravelsafeTests($travelperkOAuth),
-	new UsersTests($travelperkOAuth),
 ];
 
 foreach ($tests as $test) {
 	try {
 		$test->run();
-	} catch (Exception $e) {
+	} catch (Throwable $e) {
 		echo($e->getMessage() . "\n");
 	}
 }
